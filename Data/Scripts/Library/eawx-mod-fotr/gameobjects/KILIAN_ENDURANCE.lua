2f48c7d681fcdf63758fc0461d5dd51ab12f4b3c
Eleian <swjacklee@gmail.com>
2025-06-04 22:48:15 +0900

    1.5 migration 10

---
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/KILIAN_ENDURANCE.lua
@@ -0,0 +1,17 @@
+return {
+	Fighters = {
+		["INTERCEPTOR"] = {
+			DEFAULT = {Initial = 1, Reserve = 4}
+		},
+		["ARC_170_OFC_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 3}
+		},
+		["BOMBER"] = {
+			DEFAULT = {Initial = 1, Reserve = 4}
+		},
+		["HEAVY_BOMBER"] = {
+			DEFAULT = {Initial = 1, Reserve = 3}
+		}
+	},
+	Scripts = {"multilayer", "fighter-spawn"}
+}
