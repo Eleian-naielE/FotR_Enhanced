7fe248c2fb2280ba70f065d2ce31bf863ef6b1bb
Eleian <swjacklee@gmail.com>
2025-08-28 19:43:44 +0900

    umbaran frigate done

--- Data/Scripts/Library/eawx-mod-fotr/gameobjects/UMBARAN_FRIGATE.lua
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/UMBARAN_FRIGATE.lua
@@ -2,7 +2,7 @@ return {
 	Ship_Crew_Requirement = 45, 
 	Fighters = {
 		["UMBARAN_FIGTHER_SQUADRON"] = {
-			DEFAULT = {Initial = 1, Reserve = 1}
+			DEFAULT = {Initial = 1, Reserve = 2}
 		}		
 	}, 
 	Scripts = {"fighter-spawn", "multilayer"}
