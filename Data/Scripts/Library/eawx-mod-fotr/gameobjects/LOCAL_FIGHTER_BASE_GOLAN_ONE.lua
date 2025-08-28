b3957a6e2bdb7751cff8aaae7d14f93d12d9b6cc
Eleian <swjacklee@gmail.com>
2025-08-28 19:44:39 +0900

    umbaran fighter configured

---
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/LOCAL_FIGHTER_BASE_GOLAN_ONE.lua
@@ -0,0 +1,20 @@
+return {
+	Fighters = {
+		["N1_SQUADRON_DOUBLE"] = {
+			DEFAULT = {Initial = 2, Reserve = 99, Region = "NABOO"}
+		},
+		["NANTEX_SQUADRON_DOUBLE"] = {
+			DEFAULT = {Initial = 1, Reserve = 99, Region = "GEONOSIS"}
+		},
+		["GEONOSIAN_CRUISER_INFLUENCE"] = {
+			DEFAULT = {Initial = 1, Reserve = 0, Region = "GEONOSIS"}
+		},
+		["FIRESPRAY_SQUADRON_DOUBLE"] = {
+			DEFAULT = {Initial = 1, Reserve = 99, Region = "MANDALORE"}
+		},
+		["UMBARAN_FIGHTER"] = {
+			DEFAULT = {Initial = 1, Reserve = 99, Region = "UMBARA"}
+		},
+	},
+	Scripts = {"fighter-spawn"},
+}
