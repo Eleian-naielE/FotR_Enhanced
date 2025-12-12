55099960a6cd51211ac9597872f1d680ba7602ca
Eleian <swjacklee@gmail.com>
2025-12-13 00:10:27 +0900

    ARC Rework Objscript fix 1

--- Data/Scripts/GameObject/ObjectScript_ARC_LimitHandler.lua
+++ Data/Scripts/GameObject/ObjectScript_ARC_LimitHandler.lua
@@ -2,15 +2,6 @@ require("PGStateMachine")
 
 function Definitions()
 	Define_State("State_Init", State_Init)
-	Owner = Object.Get_Owner()
-	
-	local check = TestValid(GlobalValue.Get("ARC_LIFETIME_LIMIT"))
-	if not check then
-		local current_era = GlobalValue.Get("CURRENT_ERA")
-		local limit_table = require("CloneArcLimitLibrary")
-		local initial_limit = limit_table[current_era]
-		GlobalValue.Set("ARC_LIFETIME_LIMIT", initial_limit)
-	end	
 end
 
 function State_Init(message)
@@ -19,6 +10,14 @@ function State_Init(message)
 	end
 
 	if message == OnEnter then
+		Owner = Object.Get_Owner()
+		local check = TestValid(GlobalValue.Get("ARC_LIFETIME_LIMIT"))
+		if not check then
+		local current_era = GlobalValue.Get("CURRENT_ERA")
+		local limit_table = require("CloneArcLimitLibrary")
+		local initial_limit = limit_table[current_era]
+		GlobalValue.Set("ARC_LIFETIME_LIMIT", initial_limit)
+		end	
         local ObjectCount = GlobalValue.Get("ARC_LIFETIME_LIMIT")
 		if ObjectCount == 0 then
 			Owner.Give_Money(Object.Get_Type().Get_Build_Cost())
