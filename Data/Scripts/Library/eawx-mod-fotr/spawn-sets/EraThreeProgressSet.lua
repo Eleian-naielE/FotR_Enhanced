d1a971cebbf2d89ab7aa61287d8018a0afc90612
Eleian <swjacklee@gmail.com>
2025-12-22 16:00:49 +0900

    Added TJ912 to Era3 Progress Set

--- Data/Scripts/Library/eawx-mod-fotr/spawn-sets/EraThreeProgressSet.lua
+++ Data/Scripts/Library/eawx-mod-fotr/spawn-sets/EraThreeProgressSet.lua
@@ -1,6 +1,7 @@
 return {
     ["REBEL"] = {
     ["UMBARA"] = {"Kalani_Team", --[[FotR_Enhanced]] "TM171_DH_Omni"},
-	["CATO_NEIMOIDIA"] = {"AutO_Providence"}
+	["CATO_NEIMOIDIA"] = {"AutO_Providence"},
+    ["SULLUST"] = {"TJ912_Recusant"}
     }
 }
