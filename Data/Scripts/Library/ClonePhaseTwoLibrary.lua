8aa4a372a0b452b4eb9699d3b6e1bf0b0c541175
Eleian <swjacklee@gmail.com>
2026-02-08 11:33:56 +0900

    vehicular assault to airborne conversion

--- Data/Scripts/Library/ClonePhaseTwoLibrary.lua
+++ Data/Scripts/Library/ClonePhaseTwoLibrary.lua
@@ -7,6 +7,7 @@ return {
 	{"Republic_74Z_Speeder_Bike_Company_Dummy", "Republic_BARC_Company"}, 
 	{"Clone_Jumptrooper_Phase_One_Company_Dummy", "Clone_Jumptrooper_Phase_Two_Company"},
 	{"Clone_Special_Ops_Company_Dummy", "Clone_Galactic_Marine_Company"},
+	{"Clone_Vehicular_Assault_Company_Dummy", "Clone_Airborne_Trooper_Company"},
 	-- Generic Commanders
 	{"Commander_Tier_I_Clone_Phase_One", "Commander_Tier_I_Clone_Phase_Two_Company"},
 	{"Commander_Tier_I_ARC_Phase_One", "Commander_Tier_I_ARC_Phase_Two_Company"}, 
