bf61f75161f9288fb74c0bd9c70f0f111cae4b82
Eleian <swjacklee@gmail.com>
2026-03-15 21:01:58 +0900

    assets from NFOW (padawn anakin, knight obi wan, autoswap function)

--- Data/Scripts/Library/eawx-utils/FotREnhancedUtil.lua
+++ Data/Scripts/Library/eawx-utils/FotREnhancedUtil.lua
@@ -13,3 +13,20 @@ function Find_Nearest_Friendly_Planet(object)
         end
     end
 end
+
+function Replace_Troops(old_unit_name, upgraded_unit_name)
+    for _,unit in pairs(Find_All_Objects_Of_Type(old_unit_name)) do
+        location = unit.Get_Planet_Location()
+        if location == nil then
+            min_distance = 1000000
+            for _,planet in pairs(FindPlanet.Get_All_Planets()) do
+                if planet.Get_Distance(unit) < min_distance and planet.Get_Owner() == player then
+                    min_distance = planet.Get_Distance(unit)
+                    location = planet
+                end
+            end
+        end
+        SpawnList({upgraded_unit_name}, location, player, true, false)
+        unit.Despawn()
+    end
+end
