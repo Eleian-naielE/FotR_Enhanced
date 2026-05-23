cd5d67b93aea13be4d988ef1dd5b21124335c409
Eleian <swjacklee@gmail.com>
2026-05-23 23:25:58 +0900

    Clone Commando, ARC P1 order changed

--- Data/Scripts/Library/ShipMarketOptions.lua
+++ Data/Scripts/Library/ShipMarketOptions.lua
@@ -118,17 +118,6 @@ return {
 			faction_object = Find_Player("Empire"),
 			news_colour = {r = 250, g = 44, b = 44},
 			list = {
-				["ARC_PHASE_ONE_COMPANY"] = {
-					locked = true,
-					gc_locked = false,
-					amount = 0,
-					chance = 0,
-					perception_modifier = nil,
-					associtaion = nil,
-					readable_name = "Alpha-Class ARC Trooper Platoon (Phase I)",
-					text_requirement = "",
-					order = 1,
-				},
 				--Always Available
 				["CLONE_COMMANDO_REWORK_COMPANY"] = {
 					locked = true,
@@ -139,9 +128,20 @@ return {
 					associtaion = nil,
 					readable_name = "Clone Commando Squad",
 					text_requirement = "",
-					order = 2,
+					order = 1,
 				},
 				--Phase 1
+				["ARC_PHASE_ONE_COMPANY"] = {
+					locked = true,
+					gc_locked = false,
+					amount = 0,
+					chance = 0,
+					perception_modifier = nil,
+					associtaion = nil,
+					readable_name = "Alpha-Class ARC Trooper Platoon (Phase I)",
+					text_requirement = "",
+					order = 2,
+				},
 				["CLONE_SPECIAL_OPS_COMPANY"] = {
 					locked = true,
 					gc_locked = false,
