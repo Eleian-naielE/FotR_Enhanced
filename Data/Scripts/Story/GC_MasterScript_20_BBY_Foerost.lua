1672745e7abb9211d29c18c7a37d67df0fa44819
Eleian <swjacklee@gmail.com>
2026-06-06 23:57:54 +0900

    Gann 2

--- Data/Scripts/Story/GC_MasterScript_20_BBY_Foerost.lua
+++ Data/Scripts/Story/GC_MasterScript_20_BBY_Foerost.lua
@@ -341,6 +341,7 @@ function Generic_Story_Set_Up()
 
 	StoryUtil.SpawnAtSafePlanet("ALSAKAN", p_republic, StoryUtil.GetSafePlanetTable(), {"Trachta_Venator","Gentis_Team","Armand_Isard_Team"})
 	StoryUtil.SpawnAtSafePlanet("RENDILI", p_republic, StoryUtil.GetSafePlanetTable(), {"Dodonna_Ardent","Screed_Arlionne","Victory_I_Fleet_Star_Destroyer","Victory_I_Fleet_Star_Destroyer"})
+	StoryUtil.SpawnAtSafePlanet("ODIK", p_republic, StoryUtil.GetSafePlanetTable(), {"Gann_Team"}) -- FotR_Enhanced
 
 	p_republic.Unlock_Tech(Find_Object_Type("Victory_I_Fleet_Star_Destroyer"))
 	p_republic.Unlock_Tech(Find_Object_Type("Gladiator_I"))
@@ -366,7 +367,7 @@ function CIS_Story_Set_Up()
 		"Dodonna_Ardent","Screed_Arlionne","Bengila_Urlan_Team",
 		"Victory_I_Fleet_Star_Destroyer","Victory_I_Fleet_Star_Destroyer","Victory_I_Fleet_Star_Destroyer", "Victory_I_Fleet_Star_Destroyer","Victory_I_Fleet_Star_Destroyer","Victory_I_Fleet_Star_Destroyer"}
 	)
-	--StoryUtil.SpawnAtSafePlanet("ODIK", p_republic, StoryUtil.GetSafePlanetTable(), {"Cinzero_Gann_Team"})
+	StoryUtil.SpawnAtSafePlanet("ODIK", p_republic, StoryUtil.GetSafePlanetTable(), {"Gann_Team"}) -- FotR_Enhanced
 
 	SpawnList({"Invincible_Cruiser", "Invincible_Cruiser", "Invincible_Cruiser"}, FindPlanet("Alsakan"), p_republic, false, false)
 
@@ -606,7 +607,7 @@ function Rep_Story_Set_Up()
 
 	StoryUtil.SpawnAtSafePlanet("ALSAKAN", p_republic, StoryUtil.GetSafePlanetTable(), {"Trachta_Venator","Gentis_Team"})
 	StoryUtil.SpawnAtSafePlanet("SARAPIN", p_republic, StoryUtil.GetSafePlanetTable(), {"Bengila_Urlan_Team"})
-	--StoryUtil.SpawnAtSafePlanet("ODIK", p_republic, StoryUtil.GetSafePlanetTable(), {"Cinzero_Gann_Team"})
+	StoryUtil.SpawnAtSafePlanet("ODIK", p_republic, StoryUtil.GetSafePlanetTable(), {"Gann_Team"}) -- FotR_Enhanced
 
 	if (GlobalValue.Get("Foerost_Rep_GC_Version") == 1) then
 		p_republic.Unlock_Tech(Find_Object_Type("Lancer_Frigate_Prototype"))
