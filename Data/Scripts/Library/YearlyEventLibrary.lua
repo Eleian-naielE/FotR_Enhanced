afa59ba731afbd5a1823690984a16cc72d536af1
Eleian <swjacklee@gmail.com>
2026-06-09 23:39:00 +0900

    ARC Research, Knighting Event 1

--- Data/Scripts/Library/YearlyEventLibrary.lua
+++ Data/Scripts/Library/YearlyEventLibrary.lua
@@ -1,6 +1,8 @@
 return {
 	["22 BBY"] = {
 		{month = 7, name = "PROVIDENCE_RESEARCH"},
+		{month = 7, name = "ARC_STASIS_UNLOCK"},
+		{month = 9, name = "KNIGHTING_CEREMONY"},
 		{month = 11, name = "COMMAND_STAFF_RETURN", params = {{"Ahsoka"}, 3, true}},
 		{month = 10, name = "VENATOR_RESEARCH"},
 		--{month = 11, name = "UTAT_RESEARCH"}
