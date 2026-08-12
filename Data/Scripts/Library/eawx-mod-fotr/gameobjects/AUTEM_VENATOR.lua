89a2e9a3147ec37ba5bd8619d34d1a7a5591b80a
Eleian-naielE <swjacklee@gmail.com>
2026-08-12 23:37:59 +0900

    Wieler Turret Alt output Ability, Autem Fighter loadout changed

---
+++ Data/Scripts/Library/eawx-mod-fotr/gameobjects/AUTEM_VENATOR.lua
@@ -0,0 +1,17 @@
+return {
+	Fighters = {
+		["ARC_170_OFC_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 4}
+		},
+		["CLOAKSHAPE_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 3}
+		},
+		["NTB_630_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 4}
+		},
+		["SCURRG_H6_PROTOTYPE_SQUADRON"] = {
+			DEFAULT = {Initial = 1, Reserve = 3}
+		}
+	},
+	Scripts = {"multilayer", "fighter-spawn"}
+}
