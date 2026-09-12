0ce621007f19047261e651d4d1eda532bb6e8539
Eleian-naielE <swjacklee@gmail.com>
2026-09-12 21:20:52 +0900

    IA Spawn Land 1

--- Data/Scripts/Library/IA_Spawn_Generic.lua
+++ Data/Scripts/Library/IA_Spawn_Generic.lua
@@ -28,3 +28,32 @@ function IA_Spawn(dummy_name, script_name, marker_name_in, faction_name_in)
 
 	ScriptExit()
 end
+
+function IA_Spawn_Land(dummy_name, script_name, marker_name_in, faction_name_in)
+	if Get_Game_Mode() ~= "Land" then
+		ScriptExit()
+	end
+
+	local spawn_object_type = Find_Object_Type(string.sub(dummy_name,4)) --Trim "IA_" off the front
+
+	local marker_name = ""
+	if marker_name_in then
+		marker_name = marker_name_in
+	else
+		local pos_s, pos_e = string.find(script_name,"Add_InstantActionUnit_Land_")
+		marker_name = string.sub(script_name,pos_e+1)
+	end
+
+	local faction_name = GlobalValue.Get("IA_FACTION")
+	if faction_name then
+		--this space intentionally left blank
+	elseif faction_name_in then
+		faction_name = faction_name_in
+	else
+		faction_name = string.gsub(marker_name,"%d","")
+	end
+
+	Spawn_Unit(spawn_object_type, Find_First_Object("INSTANTACTION_LAND_MARKER_"..string.upper(marker_name)), Find_Player(faction_name))
+
+	ScriptExit()
+end
